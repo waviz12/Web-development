50842d611c8a2513961f8c072eeef67c16f9f2bc
daniayr <sakenulydaniar0@gmail.com>
2026-02-24 14:23:27 +0500

    Add Task2 online-store project

---
+++ Task2/online-store/src/app/components/product-card/product-card.ts
@@ -0,0 +1,49 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Product } from '../../models/product.model';
+
+@Component({
+  selector: 'app-product-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './product-card.html',
+  styleUrls: ['./product-card.css']
+})
+export class ProductCardComponent {
+  @Input() product!: Product;
+
+  formatPrice(price: number): string {
+    return new Intl.NumberFormat('ru-RU').format(price) + ' ₸';
+  }
+
+  shareOnWhatsApp() {
+    const text = `Посмотрите этот товар: ${this.product.name}`;
+    const url = `https://wa.me/?text=${encodeURIComponent(text + ' ' + this.product.link)}`;
+    window.open(url, '_blank');
+  }
+
+  shareOnTelegram() {
+    const url = `https://t.me/share/url?url=${encodeURIComponent(this.product.link)}&text=${encodeURIComponent(this.product.name)}`;
+    window.open(url, '_blank');
+  }
+
+  getStars() {
+    const stars = [];
+    const fullStars = Math.floor(this.product.rating);
+    const hasHalfStar = this.product.rating % 1 >= 0.5;
+    
+    for (let i = 0; i < fullStars; i++) {
+      stars.push('full');
+    }
+    
+    if (hasHalfStar) {
+      stars.push('half');
+    }
+    
+    while (stars.length < 5) {
+      stars.push('empty');
+    }
+    
+    return stars;
+  }
+}
